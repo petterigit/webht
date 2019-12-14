57865eb65f5d08c24913a77d2678c9923e413806
petterigit <p-makela@outlook.com>
2019-12-14 14:33:06 +0200

    V6

--- src/App.js
+++ src/App.js
@@ -1,19 +1,93 @@
 import React from 'react';
+import axios from 'axios';
 
 import Feed from "./components/Feed.js"
 
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      data: [],
+      id: 0,
+      user: "",
+      text: "",
+      intervalIsSet: false,
+      idToDelete: null,
+      idToUpdate: null,
+      objectToUpdate: null,
+    };
+
+    /* Bind DB functions so we can pass them to and use them with children */
+
+    this.getDataFromDB = this.getDataFromDB.bind(this)
+    this.putDataToDB = this.putDataToDB.bind(this)
+    this.deleteFromDB = this.deleteFromDB.bind(this)
+
   }
+
+  /* Two react lifecycle functions to initialize and keep up with database */
+  componentDidMount() {
+    this.getDataFromDB();
+    if (!this.state.intervalIsSet) {
+      let interval = setInterval(this.getDataFromDB, 1000);
+      this.setState({ intervalIsSet: interval });
+    }
+  }
+  componentWillUnmount() {
+    if (this.state.intervalIsSet) {
+      clearInterval(this.state.intervalIsSet);
+      this.setState({ intervalIsSet: null });
+    }
+  }
+
+  /* Database access methods:
+        GET POST DELETE*/
+   getDataFromDB = () => {
+     fetch('http://localhost:3001/api/getData')
+       .then((data) => data.json())
+       .then((res) => this.setState({ data: res.data }));
+   };
+   putDataToDB = (user, text) => {
+     let currentIds = this.state.data.map((data) => data.id);
+     let idToBeAdded = 0;
+     while (currentIds.includes(idToBeAdded)) {
+       ++idToBeAdded;
+     }
+     console.log("putDataToDB: " + idToBeAdded + " " + user + " " + text)
+     axios.post('http://localhost:3001/api/putData', {
+       id: idToBeAdded,
+       user: user,
+       text: text
+     });
+   };
+   deleteFromDB = (idTodelete) => {
+     parseInt(idTodelete);
+     let objIdToDelete = null;
+     this.state.data.forEach((dat) => {
+       if (dat.id === idTodelete) {
+         objIdToDelete = dat._id;
+       }
+     });
+     axios.delete('http://localhost:3001/api/deleteData', {
+       data: {
+         id: objIdToDelete,
+       },
+     });
+   };
+
+
+   /* Render UI */
   render() {
     return (
       <div className="App">
-        <Feed />
+        <Feed
+          getDataFromDb={this.getDataFromDB}
+          putDataToDB={this.putDataToDB}
+          deleteFromDB={this.deleteFromDB}
+          data={this.state.data}
+          />
       </div>
     );
   }
 }
-
 export default App;
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,60 +2,39 @@ import React from "react"
 
 import FeedItem from "./feed/FeedItem.js"
 import Input from "./feed/Input.js"
-import feedData from "./feed/FeedData.js"
 
 
 class Feed extends React.Component {
   constructor() {
     super()
     this.state = {
-      newData: {},
-      feedList: feedData,
-      id: 5
+      newData: {}
     }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(e) {
-    console.log("Update data");
-    console.log(e.target);
-    //console.log(e.target.querySelector("#textArea").value);
-    //console.log(e.target.querySelector("#nameInput").value);
-
+    e.preventDefault();
+    console.log("handleSubmit");
     let textInput = e.target.querySelector("#textArea").value;
     let nameInput = e.target.querySelector("#nameInput").value;
-
-    const paramData = {
-      text: textInput,
-      id: this.state.id,
-      user: nameInput
-    }
-
-    e.preventDefault();
-
-    this.setState({newData: paramData})
-    this.setState(prevState => {
-      const feedList = prevState.feedList.concat(prevState.newData)
-
-      return {
-        feedList,
-        newData: {},
-        id: prevState.id + 1
-      }
-    })
+    this.props.putDataToDB(nameInput, textInput);
     }
 
   render() {
-    const feedList = this.state.feedList.map(data => <FeedItem key={data.id} user={data.user} text={data.text} />)
+    const feedList = this.props.data.map(data =>
+      <FeedItem
+        key={data.id}
+        user={data.user}
+        text={data.text}
+        />)
 
     return (
       <div className="Feed">
         <Input handleSubmit={this.handleSubmit} />
         {feedList}
-
       </div>
-    )
-    }
+    )}
   }
 
 export default Feed
